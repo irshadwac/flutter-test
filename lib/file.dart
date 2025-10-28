19489aff793a6c98d54bc12ef34eeee3eee1e64f
Irshad <irshad.kp@wacmob.com>
2025-10-28 09:11:10 +0530

    feat : init

---
+++ lib/file.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// Doc should be added on this so this widget is a
+/// dummy widget
+class MyWidget extends StatefulWidget {
+  /// Doc should be added on this so this widget is a
+  /// dummy widget
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
